ff6e7552bcc46a4c384bbc365af808de2c5ee849
Krzysztof Rychlicki-Kicior <krzysztof_rychlicki@LR.WSINF.EDU.PL>
2017-12-14 10:31:04 +0100

    Another fix

--- src/CwiczenieMain.java
+++ src/CwiczenieMain.java
@@ -3,11 +3,8 @@ public class CwiczenieMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-<<<<<<< HEAD
 		Factorial fact = new Factorial(6);
 		System.out.println(fact.calculate());
-=======
->>>>>>> hotfix/ceochange
 		Fibonacci f = new Fibonacci(30);
 		System.out.println(f.calculate());
 	}
