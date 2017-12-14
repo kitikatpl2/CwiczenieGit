bf018ed4492b0965da0852e34b48ec5ce61895e5
Krzysztof Rychlicki-Kicior <krzysztof_rychlicki@LR.WSINF.EDU.PL>
2017-12-14 09:49:08 +0100

    Fibonacci ready

--- src/CwiczenieMain.java
+++ src/CwiczenieMain.java
@@ -3,6 +3,8 @@ public class CwiczenieMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+		Fibonacci f = new Fibonacci(10);
+		System.out.println(f.calculate());
 	}
 
 }
