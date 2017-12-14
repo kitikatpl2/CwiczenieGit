419f0d444b362b5fc858ea8b854120dca1a2b948
Krzysztof Rychlicki-Kicior <krzysztof_rychlicki@LR.WSINF.EDU.PL>
2017-12-14 09:49:27 +0100

    Small fix to Fib

--- src/CwiczenieMain.java
+++ src/CwiczenieMain.java
@@ -3,7 +3,7 @@ public class CwiczenieMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		Fibonacci f = new Fibonacci(10);
+		Fibonacci f = new Fibonacci(12);
 		System.out.println(f.calculate());
 	}
 
